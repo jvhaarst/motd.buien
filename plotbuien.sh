fdc65657b276e050caf6db273bd101247131598a
Jan van Haarst <jan@vanhaarst.net>
2012-12-22 14:08:20 +0100

    Add comments to plot commands, and info from Wikipedia on rain classifications.

--- plotbuien.sh
+++ plotbuien.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # This script plots the predicted rainfall, as described at http://gratisweerdata.buienradar.nl/
 # TODO : do not create tempfile, but only create a new file if old one is stale
+# According to http://en.wikipedia.org/wiki/Rain#Intensity , intensity of rain is this:
+#	Light rain — when the precipitation rate is < 2.5 millimetres per hour
+#	Moderate rain — when the precipitation rate is between 2.5 millimetres - 7.6 millimetres or 10 millimetres per hour
+#	Heavy rain — when the precipitation rate is > 7.6 millimetres per hour, or between 10 millimetres and 50 millimetres per hour
+#	Violent rain — when the precipitation rate is > 50 millimetres per hour
+
 LATITUDE=51.985
 LONGITUDE=5.665
 XSIZE=72
@@ -13,14 +19,15 @@ curl --silent "http://gps.buienradar.nl/getrr.php?lat=${LATITUDE}&lon=${LONGITUD
 # Plot the data in the tempfile
 gnuplot 2>/dev/null << EOF
 	set term dumb ${XSIZE} ${YSIZE};
-	set key off;
-	set xdata time;
-	set timefmt "%H:%M";
-	set format x "%H:%M";
-	set ytics nomirror
-	unset border
-	set yrange [0:]
-	plot "${TMPFILE}" using 2:(10**((\$1-109)/32)) with impulse;
+	set key off;													# Remove legend
+	set xdata time;													# x-axis is time data
+	set timefmt "%H:%M";											# Format of input data
+	set format x "%H:%M";											# How do we show the time
+	set ytics nomirror												# Remove right tics
+	unset border													# Remove border from plot
+	set yrange [0:]													# Let the plot start at 0
+	set offsets graph 0, 0, 0.05, 0.05								# Add some space around the points
+	plot "${TMPFILE}" using 2:(10**((\$1-109)/32)) with impulse;	# Plot the data, calculate mm from measurements
 EOF
 
 # Remove tempfile
