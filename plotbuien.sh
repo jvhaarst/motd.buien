955c2e0f8eb176b0e9f25f9c905a3aa00269eb19
Jan van Haarst <jan@vanhaarst.net>
2012-12-22 12:46:14 +0100

    Improved look of plot, and made size a variable

--- plotbuien.sh
+++ plotbuien.sh
@@ -3,6 +3,8 @@
 # TODO : do not create tempfile, but only create a new file if old one is stale
 LATITUDE=51.985
 LONGITUDE=5.665
+XSIZE=72
+YSIZE=30
 # Create tempfile to hold the data
 tempfoo=`basename $0`
 TMPFILE=`mktemp /tmp/${tempfoo}.XXXXXX` || exit 1
@@ -10,13 +12,15 @@ TMPFILE=`mktemp /tmp/${tempfoo}.XXXXXX` || exit 1
 curl --silent "http://gps.buienradar.nl/getrr.php?lat=${LATITUDE}&lon=${LONGITUDE}" | sed 's/|/ /'  > $TMPFILE
 # Plot the data in the tempfile
 gnuplot 2>/dev/null << EOF
-	set term dumb 72 20;
+	set term dumb ${XSIZE} ${YSIZE};
 	set key off;
 	set xdata time;
 	set timefmt "%H:%M";
 	set format x "%H:%M";
+	set ytics nomirror
+	unset border
 	set yrange [0:]
-	plot "${TMPFILE}" using 2:(10**((\$1-109)/32));
+	plot "${TMPFILE}" using 2:(10**((\$1-109)/32)) with impulse;
 EOF
 
 # Remove tempfile
